d56c795951319768ba432048888c91f00a6ff8b9
Keith <113943336+uwugamergirluwu@users.noreply.github.com>
2025-09-25 10:47:04 -0400

    Update TwilioTemplate.py

--- TwilioTemplate.py
+++ TwilioTemplate.py
@@ -4,13 +4,21 @@ from twilio.rest import Client
 
 account_sid = 'ACCOUNT_SID'  #from twilio dashboard
 auth_token = 'AUTH_TOKEN'    #from twilio dashboard
+dest_number = '+1_DESTINATION_NUMBER' #verified phone number
+twilio_number = '+1_TWILIO_NUMBER'#from twilio dashboard
 client = Client(account_sid, auth_token)
 
-#Make the call
+#Make a call
 call = client.calls.create(
     url='', #link to TwiML file (Twilio Markup Language), programs voice message
-    to='+1_DESTINATION_NUMBER',
-    from_='+1_TWILIO_NUMBER' #from twilio dashboard
+    to=dest_number,
+    from_= twilio_number
 )
 
+#Send a message
+message = client.messages.create(
+    body="MESSAGE"
+    to=dest_number,
+    from_= twilio_number #from twilio dashboard
+)
 print("Call SID:", call.sid)

0e0d185217035eed1a42d9727d0afc6c204187fd
Aaditya Shah <aaditya.shah@hotmail.com>
2025-10-11 03:40:31 -0400

    remove redundant dependecies

--- src/pages/alerts/alerts.tsx
+++ src/pages/alerts/alerts.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import MoonToggle from "../../components/MoonToggle";
 
 import logoUrl from "../../assets/Drivesence-logo.png";

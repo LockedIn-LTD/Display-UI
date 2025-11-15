fd3ca980633fc47f4b96b90c2eaa04c104fcda41
Keith <keithtran02@gmail.com>
2025-11-15 00:24:41 -0500

    Fix useTheme export in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { ThemeProvider, useTheme } from "./theme/ThemeProvider";
-
+import { ThemeProvider } from "./theme/ThemeProvider";
+import { useTheme } from "./theme/theme-context";
 import LoginPage from "./pages/login/login";
 import DriverSelection from "./pages/driver-selection/driver-selection";
 import StatusProcessing from "./pages/status-processing/status-processing";
